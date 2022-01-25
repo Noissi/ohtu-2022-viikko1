eb07ddb3342984e89c72e387d764e92a6f9bea73
Peruna <noora.matilainen@helsinki.fi>
2022-01-25 19:12:59 +0200

    viikko2 t3-t4

--- src/varasto.py
+++ src/varasto.py
@@ -1,17 +1,10 @@
 class Varasto:
-    def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
+    def __init__(self, tilavuus, alku_saldo=0):
+        self.tilavuus = max(tilavuus, 0)
+
+        if alku_saldo <= tilavuus:
             # mahtuu
-            self.saldo = alku_saldo
+            self.saldo = max(alku_saldo, 0)
         else:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
@@ -21,17 +14,15 @@ class Varasto:
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
-        if maara < 0:
-            return
-        if maara <= self.paljonko_mahtuu():
+        if 0 < maara <= self.paljonko_mahtuu():
             self.saldo = self.saldo + maara
-        else:
+        elif maara > self.paljonko_mahtuu():
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +31,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
